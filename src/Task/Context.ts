4c614d9a1155e12f02f54ec4f6a268391ad02c4c
Ralph Schuler <ralph@inhaus.ai>
2022-04-25 17:20:19 +0200

    fix

--- src/Task/Context.ts
+++ src/Task/Context.ts
@@ -2,7 +2,7 @@ export class TaskContext {
   private context: { [key: any]: any };
 
   public get<T>(key: any): T {
-    return this.context[key];
+    return this.context[key] as T;
   }
 
   public set<T>(key: any, value: T): void {
